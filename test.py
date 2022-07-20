920c70e74ca5837bfb4f268dddf79f159e561dd3
wany0824 <43168474+wany0824@users.noreply.github.com>
2022-07-20 17:22:55 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,219 @@
+import sys
+
+sys.setrecursionlimit(15000)
+import os
+import random
+from PIL import Image
+import torch
+import numpy as np
+import torch.utils.data
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torch.nn import functional as F
+from torchvision import transforms
+from tqdm import tqdm
+import glob
+import argparse
+from sklearn.metrics import roc_auc_score
+from network.vit_16_base_feat_middle_gal_v2 import vit_base_patch16_224
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', default='vit')
+parser.add_argument('--dataset', default ="", help='path to dataset')
+parser.add_argument('--originroot', default ="", help='path to root original')
+parser.add_argument('--test_set', default ='./dataset/test_stand.json', help='test set')
+parser.add_argument('--src', default="Deepfakes Face2Face FaceSwap NeuralTextures")
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+parser.add_argument('--batchsize', type=int, default=128, help='input batch size')
+parser.add_argument('--framenum', type=int, default=110)
+parser.add_argument('--imageSize', type=int, default=224, help='the height / width of the input image to network')
+parser.add_argument('--patch', type=int, default=16)
+parser.add_argument('--outf', default='/home/wany/project/xception-FF/FFall/ckpt1_lr0.00002_all/', help='folder to output log and load model checkpoints')
+parser.add_argument('--testmodel', default="model_bestacc.pt")
+parser.add_argument('--record_error', default=True)
+parser.add_argument('--attn_blk', type=str, default='8 9 10 11 12')
+parser.add_argument('--feat_blk', type=int, default=6)
+parser.add_argument('--k_weight', type=float, default=12.0)
+parser.add_argument('--k_thr', type=float, default=0.7)
+parser.add_argument('--step', type=float, default=1.0)
+parser.add_argument('--is_progressive', type=int, default=1)
+opt = parser.parse_args()
+print(opt)
+
+opt.attn_blk = [int(i) for i in opt.attn_blk.split()]
+
+class TestSet(Dataset):
+    def __init__(self, test_frames, face_path):
+        random.seed(8664)
+        self.facepath = face_path
+        self.frames = test_frames
+        self.transform_xcp = transforms.Compose([
+            transforms.Resize((opt.imageSize, opt.imageSize)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)])
+        self.imgs = self._get_img_list()
+        
+
+    def _get_img_list(self):
+        imgs = []
+        filelist = glob.glob(os.path.join(self.facepath, '*_0.png'))
+        try:
+            for path in filelist[:self.frames]:
+                imgs.append(path)
+        except(IndexError):
+            pass       
+    
+        return imgs
+
+    def __len__(self):
+        return(len(self.imgs))
+
+    def _load_image(self, path):
+        image = Image.open(path).convert('RGB')
+        return image
+    
+    def __getitem__(self,idx):
+        img_name = self.imgs[idx]
+        image = self._load_image(img_name)
+        if 'c40' in img_name:
+            H,W = image.size
+            centercrop = transforms.CenterCrop((int(H/1.3), int(W/1.3)))
+            image = centercrop(image)
+        image = self.transform_xcp(image)
+        return image
+
+def get_datalist_stand(datapath):
+    with open(datapath, 'r') as f:
+        data_dict = json.load(f)
+    output = []
+    for pair in data_dict:
+        name1, name2 = pair[0], pair[1]
+        output.append([name1, '0'])
+        output.append([name2, '0'])
+        output.append([name1 + '_' + name2, '1'])
+        output.append([name2 + '_' + name1, '1'])
+    return output
+
+
+def test(model, testroot, origin, testlist, test_frame, batchsize, record_error):
+    video_list = get_datalist_stand(testlist)
+    tol_label = np.array([])
+    tol_label_frame = np.array([])
+    tol_pred = np.array([])
+    tol_pred_frame = np.array([])
+   
+    if record_error:
+        error_vid_file = open(os.path.join(opt.outf, 'test_error_message.txt'), 'w')
+    for item in tqdm(video_list):
+        vid_name, label = item
+        if label == "0":
+            face_path = os.path.join(origin, vid_name)
+            testdataset = TestSet(test_frames=test_frame, face_path=face_path)
+            testloder = DataLoader(
+                testdataset,
+                batch_size=test_frame,
+                shuffle=False,
+                num_workers=2,
+                pin_memory=True
+            )
+            pred = test_video(testloder, model)
+            ## calculate accuracy in frame level
+            target = [float(label)] * len(pred)
+            tol_pred_frame = np.concatenate((tol_pred_frame, pred))
+            tol_label_frame = np.concatenate((tol_label_frame, target))
+            ## calculate accuracy in video level
+            preds = np.array([np.mean(pred)], dtype=float)
+            label = np.array([float(label)], dtype=float)
+            tol_label = np.concatenate((tol_label, label))
+            tol_pred = np.concatenate((tol_pred, preds))
+            if record_error:
+                pred_label = 1.0 if preds[0] >= 0.5 else 0.0
+                if pred_label != label[0]:
+                    txt = vid_name.split('/')[-1] + ' ' + str(preds[0]) + '\n'
+                    error_vid_file.write(txt) 
+        else:
+            # forgery = ["Deepfakes", "Face2Face", "FaceSwap", "NeuralTextures"]
+            forgery = [s for s in opt.src.split()]
+            for f in forgery:
+                face_path = os.path.join(testroot.replace('src', f), vid_name)
+                testdataset = TestSet(test_frames=test_frame, face_path=face_path)
+                testloder = DataLoader(
+                    testdataset,
+                    batch_size=test_frame,
+                    shuffle=False,
+                    num_workers=2,
+                    pin_memory=True
+                )
+                
+                pred = test_video(testloder, model)
+                ## calculate accuracy in frame level
+                target = [float(label)] * len(pred)
+
+                tol_pred_frame = np.concatenate((tol_pred_frame, pred))
+                tol_label_frame = np.concatenate((tol_label_frame, target))
+                ## calculate accuracy in video level
+                preds = np.array([np.mean(pred)], dtype=float)
+                label = np.array([float(label)], dtype=float)
+                tol_label = np.concatenate((tol_label, label))
+                tol_pred = np.concatenate((tol_pred, preds))
+                if record_error:
+                    pred_label = 1.0 if preds[0] >= 0.5 else 0.0
+                    if pred_label != label[0]:
+                        txt = f + '/'+ vid_name + ' ' + str(preds[0]) + '\n'
+                        error_vid_file.write(txt) 
+    
+    auc = roc_auc_score(tol_label, tol_pred)
+    auc_frame  =roc_auc_score(tol_label_frame, tol_pred_frame)
+
+    top1_acc = compute_accuracy(tol_label, tol_pred)
+    top1_acc_frame = compute_accuracy(tol_label_frame, tol_pred_frame)
+    
+
+    if record_error:
+        error_vid_file.close()
+    return top1_acc_frame, auc_frame, top1_acc, auc
+
+
+def test_video(testloder, model):
+    model.eval()
+    with torch.no_grad():
+        preds = np.array([])
+        for img in testloder:
+            img = img.cuda()
+            cls_out, _, _ = model(img, step=opt.step, attn_blk=opt.attn_blk, feat_blk=opt.feat_blk, k=opt.k_weight, thr=opt.k_thr, is_progressive=opt.is_progressive)
+            prob = F.softmax(cls_out, dim=1).cpu().data.numpy()[:, 1]
+            preds = np.concatenate((preds, prob))
+
+    return preds 
+
+def compute_accuracy(target, output):
+    pred = output > 0.5
+    return np.sum(pred==target)/target.shape[0]
+
+if __name__ == '__main__':
+
+    text_writer = open(os.path.join(opt.outf, 'test.txt'), 'a')
+    net = vit_base_patch16_224(pretrained=False, num_classes=2)
+
+    print('\n')
+    print("**Testing** Get test files done!")
+    print("Testroot : " + opt.dataset)
+    print("Test Model: " + opt.outf+opt.testmodel)
+    # load model
+    net = torch.nn.DataParallel(net).cuda()
+    net_ = torch.load(os.path.join(opt.outf, opt.testmodel))
+    net.load_state_dict(net_)
+    # test model
+    test_args = test(net, opt.dataset, opt.originroot, opt.test_set, 
+                opt.framenum, opt.batchsize, opt.record_error)
+    print('\n===========Test Info===========\n')
+    print('Test ACC_frame: %6.3f' % (test_args[0]*100))
+    print('Test AUC_frame: %6.3f' % (test_args[1]*100))
+    print('Test ACC_vid: %6.3f' % (test_args[2]*100))
+    print('Test AUC_vid: %6.3f' % (test_args[3]*100))
+    print('\n===============================\n')
+
+
+        
+
---
+++ utils/utils.py
@@ -0,0 +1,136 @@
+import json
+import math
+from random import randrange
+from turtle import forward
+import numpy as np
+from numpy.core.numeric import outer
+import pandas as pd
+import torch
+import os
+import sys
+import shutil
+import torch
+import torch.nn as nn
+from sklearn.covariance import LedoitWolf
+from network.vit_16_base_feat_middle_gal_v2 import vit_base_patch16_224
+import pdb
+
+        
+def fit_inv_covariance(samples):
+    return torch.Tensor(LedoitWolf().fit(samples.cpu()).precision_).to(
+        samples.device
+    )
+
+def mahalanobis_distance(
+        values: torch.Tensor, mean: torch.Tensor, inv_covariance: torch.Tensor
+    ) -> torch.Tensor:
+        """Compute the batched mahalanobis distance.
+
+        values is a batch of feature vectors.
+        mean is either the mean of the distribution to compare, or a second
+        batch of feature vectors.
+        inv_covariance is the inverse covariance of the target distribution.
+        """
+        assert values.dim() == 2
+        assert 1 <= mean.dim() <= 2
+        assert inv_covariance.dim() == 2
+        assert values.shape[1] == mean.shape[-1]
+        assert mean.shape[-1] == inv_covariance.shape[0]
+        assert inv_covariance.shape[0] == inv_covariance.shape[1]
+
+        if mean.dim() == 1:  # Distribution mean.
+            mean = mean.unsqueeze(0)
+        x_mu = values - mean  # batch x features
+        # Same as dist = x_mu.t() * inv_covariance * x_mu batch wise
+        dist = torch.einsum("im,mn,in->i", x_mu, inv_covariance, x_mu)
+        return dist.sqrt()
+
+
+import pdb
+
+class Attention_Correlation_weight_reshape_loss(nn.Module):
+    def __init__(self, c_out, c_in, c_cross) -> None:
+        super(Attention_Correlation_weight_reshape_loss, self).__init__()
+        self.c_out = c_out
+        self.c_in = c_in
+        self.c_cross = c_cross
+        self.device = c_out.device
+    def forward(self, correlation_map_real, correlation_map_fake, fake_weight):
+        if correlation_map_real.shape[0] == 0:
+            loss_real = 0
+        else:
+            B, PP, PP = correlation_map_real.shape
+            C_matrix = torch.eye(PP).to(self.device) * (1-self.c_out) + self.c_out*torch.ones(PP).to(self.device)
+            C_matrix = C_matrix.expand(B, -1, -1)
+            loss_real = torch.sum(torch.abs(correlation_map_real - C_matrix))/(B*(PP*PP-PP))
+
+        if correlation_map_fake.shape[0] == 0:
+            loss_fake = 0
+        else:
+            B, PP, PP = correlation_map_fake.shape
+            C_matrix_fake_all = []
+            for b in range(B):
+                fake_index = torch.where(fake_weight[b].reshape(-1)>0)[0]
+                real_index = torch.where(fake_weight[b].reshape(-1)<=0)[0]
+                C_matrix_fake = torch.zeros((PP, PP)).to(self.device) + self.c_cross
+                for i in fake_index:
+                    C_matrix_fake[i, fake_index] = self.c_in
+                for j in real_index:
+                    C_matrix_fake[j, real_index] = self.c_out
+                C_matrix_fake_all.append(C_matrix_fake)
+            C_matrix_fake_all = torch.stack(C_matrix_fake_all).to(self.device)
+            # C_matrix_fake = C_matrix_fake.expand(B, -1, -1).cuda()
+            loss_fake = torch.sum(torch.abs(correlation_map_fake - C_matrix_fake_all))/(B*(PP*PP-PP))
+
+        loss = loss_fake + loss_real
+        return loss
+
+
+def estimate_MVG(feat_tensorlist_real, feat_tensorlist_fake):
+    ## for compute MVG parameter
+    feat_tensorlist = torch.cat(feat_tensorlist_real, dim=0).cuda()
+    inv_covariance_real = fit_inv_covariance(feat_tensorlist_real).cpu()
+    mean_real = feat_tensorlist.mean(dim=0).cpu()  # mean features.
+
+    feat_tensorlist_fake = torch.cat(feat_tensorlist_fake, dim=0).cuda()
+    inv_covariance_fake = fit_inv_covariance(feat_tensorlist_fake).cpu()
+    mean_fake = feat_tensorlist_fake.mean(dim=0).cpu()  # mean features.
+
+    return mean_real, mean_fake, inv_covariance_real, inv_covariance_fake
+
+
+if __name__ == "__main__":
+    ## 
+    model = vit_base_patch16_224(pretrained=True, num_classes=2)
+    model.train()
+    ## create your own dataloader
+    train_iter = iter(dataloader_train)
+    img_data, img_label = train_iter.next()
+    img_label = img_label.numpy().astype(np.float)
+
+    [c_cross, c_in, c_out] = [nn.Parameter(torch.tensor(float(i)).cuda()) for i in opt.c_initilize.split()] 
+    attention_loss = Attention_Correlation_weight_reshape_loss(c_out=c_out, c_in=c_in, c_cross=c_cross)
+    classes, feat_patch, attn_map = model(img_data)
+    B, H, W, C = feat_patch.size()
+
+    ## acquire the mean attention map from multiple layers of the network
+    realindex = np.where(img_label==0.0)[0]
+    attn_map_real = torch.sigmoid(torch.mean(attn_map[realindex,:, 1:, 1:], dim=1))
+    fakeindex = np.where(img_label==1.0)[0]
+    attn_map_fake = torch.sigmoid(torch.mean(attn_map[fakeindex,:, 1:, 1:], dim=1))
+
+    ## forgery localization 
+    ## should accumulate the real/fake samples every iteration to acquire feat_tensorlist_real, feat_tensorlist_fake, then update MVG
+    feat_tensorlist_real.append(feat_patch[realindex[:4]].reshape(B*H*W, C).cpu().detach())
+    feat_tensorlist_fake.append(feat_patch[realindex[:4]].reshape(B*H*W, C).cpu().detach())
+
+    if True: ## after training for a fix number of epoch
+        mean_real, mean_fake, inv_covariance_real, inv_covariance_fake = estimate_MVG(feat_tensorlist_real, feat_tensorlist_fake)
+    
+    maha_patch_1 = mahalanobis_distance(feat_patch.reshape((B*H*W, C)), mean_real.cuda(), inv_covariance_real.cuda())
+    maha_patch_2 = mahalanobis_distance(feat_patch.reshape((B*H*W, C)), mean_fake.cuda(), inv_covariance_fake.cuda())
+    ## index_map is the pseudo localization map
+    index_map = torch.relu(maha_patch_1 - maha_patch_2).reshape((B, H, W))
+
+    ## calculate the consistency loss
+    loss_inter_frame = attention_loss(attn_map_real, attn_map_fake, index_map[fakeindex,:])
